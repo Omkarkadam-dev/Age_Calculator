b594755b26e3c27d666949e4f7b283d3e3ac6a35
Omkarkadam-dev <omkarkadam1115@gmail.com>
2024-06-26 23:00:13 +0530

    update Script.js(JavaScript)

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-let inputBtn = document.querySelector("age-dob-input");
-let btn = document.querySelector(".btn");
-let ageResult = document.querySelector(".result");
-
+let inputBtn=document.querySelector(".age-dob-input"
+);
+let btn=document.querySelector(".btn");
+let ageResult=document.querySelector(".result");
 
 
 btn.addEventListener("click",()=>{
     if(inputBtn.value===""){
-        alert("Please enter your age or date of birth");
-    } else {
+        alert("please select your date of birth");
+    }else{
         console.log(inputBtn.value);
-        let dob = new Date(inputBtn.value);
+        let dob=new Date(inputBtn.value);
         console.log(dob);
 
-        let dob_year = dob.getFullYear();
+
+        let dob_year=dob.getFullYear();
         console.log(dob_year);
 
+
         //current
-        let now = new Date();
+        let now=new Date();
         console.log(now);
-        let now_year = nom.getFullYear();
+        let now_year=now.getFullYear();
         console.log(now_year);
 
-        let age = now_year - dob_year;
-        console.log(age); 
 
-        ageResult.textContent = 'Your age is : ${age}';
+        let age=now_year - dob_year;
+        console.log(age);
 
+        ageResult.textContent=`your age is : ${age}`;
     }
 })
